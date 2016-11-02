ae97f6d2142bb963b9d9f1b19d88d1f39854d9e7
Indrashish Ghosh <mail@indrashishghosh.com>
2016-11-02 17:46:59 +0530

    Updated deploy task

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -1,17 +1,17 @@
 'use strict';
 
 import gulp from 'gulp';
+import exit from 'gulp-exit'
 import ghPages from 'gulp-gh-pages';
 
 gulp.task('deploy', (done) => {
 
-  return gulp.src('./build/**/*')
-
+  return gulp
+  .src('./build/**/*')
   .pipe(ghPages({
-     'remoteUrl' : 'git@github.com:mailtokamalnayan/wannabuy.git'
-  })).on('end', () => {
-  	done();
-  })
+    'remoteUrl' : 'git@github.com:mailtokamalnayan/wannabuy.git'
+  }))
+  .pipe(exit())
 
-  
+  done()
 });
